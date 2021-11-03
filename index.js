9226e0177ffd8e51a153a5a91b6835d1426314c4
Menchu MV <mcmvillalobos@gmail.com>
2021-11-03 15:37:22 +0100

    Fixed checkInput function to only accept 2 numbers

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const chalk = require("chalk");
+
 const input = process.argv.slice(2);
 
 const operation = (num1, num2) => {
@@ -6,20 +8,28 @@ const operation = (num1, num2) => {
   const multiplication = num1 * num2;
   const division = num1 / num2;
 
-  return `Resultados:
-  ${num1} + ${num2} = ${sum}
-  ${num1} - ${num2} = ${substraction}
-  ${num1} * ${num2} = ${multiplication}
-  ${num1} / ${num2} = ${division}`;
+  return `${chalk.magenta("Resultados:")}
+  ${chalk.blueBright(num1)} ${chalk.magenta("+")} ${chalk.blueBright(
+    num2
+  )} ${chalk.magenta("=")} ${chalk.yellow(sum)}
+  ${chalk.blueBright(num1)} ${chalk.magenta("-")} ${chalk.blueBright(
+    num2
+  )} ${chalk.magenta("=")} ${chalk.yellow(substraction)}
+  ${chalk.blueBright(num1)} ${chalk.magenta("*")} ${chalk.blueBright(
+    num2
+  )} ${chalk.magenta("=")} ${chalk.yellow(multiplication)}
+  ${chalk.blueBright(num1)} ${chalk.magenta("/")} ${chalk.blueBright(
+    num2
+  )} ${chalk.magenta("=")} ${chalk.yellow(division)}`;
 };
 
 const checkInput = (array) => {
   if (
-    array.length === 1 ||
+    array.length !== 2 ||
     Number.isNaN(+array[0]) ||
     Number.isNaN(+array[1])
   ) {
-    console.log("Introduce dos números");
+    console.log(chalk.red("Error: Introduce dos números"));
     process.exit(9);
   }
 };
